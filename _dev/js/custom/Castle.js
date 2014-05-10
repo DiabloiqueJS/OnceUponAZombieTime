148a7ddd045b659cf94d5dda50ad9227859bc086
EmmAlvado <Micron12>
2014-05-10 18:42:25 +0200

    ezrtdyh

--- _dev/js/custom/Castle.js
+++ _dev/js/custom/Castle.js
@@ -4,41 +4,60 @@ function( datas, DE, GamePad)
 	function Castle( _screenSizes )
 	{
 		DE.GameObject.call( this, {
-	      "x": 40, "y": 400, "zindex": 1, "tag": "castle"
-	      ,"collider": new DE.CircleCollider( 20 )
+	      "x": 110, "y": 175, "zindex": 1, "tag": "castle"
+	      ,"collider": new DE.CircleCollider( 40 )
 	    } );
 
-	    var castle1 = new DE.GameObject( {
-	      "x": 40, "y": 500, "z": 5, "tag": "castle", "collider": new DE.CircleCollider( 20 )
-	    }  );
+		var life;
+	    this.add( life = new DE.GameObject( {
+	      "x": 20, "y": -150, "z": 5, "renderer": new DE.SpriteRenderer( { "spriteName": "life", "scaleY": 1, "scaleX": 0.5 } )
+	    } ) );
 
-	    var castle2 = new DE.GameObject( {
-	      "x": 40, "y": 600, "z": 5, "tag": "castle", "collider": new DE.CircleCollider( 20 )
-	    }  );
+	    this.lifeWidth  = life.renderers[0].sizes.width;
+		this.life       =  2000;
+	    this.maxlife    =  2000;
 
-	    var castle3 = new DE.GameObject( {
-	      "x": 40, "y": 700, "z": 5, "tag": "castle", "collider": new DE.CircleCollider( 20 )
-	    }  );
 
-	    var castle4 = new DE.GameObject( {
-	      "x": 40, "y": 300, "z": 5, "tag": "castle", "collider": new DE.CircleCollider( 20 )
-	    } );
-
-	    var castle5 = new DE.GameObject( {
-	      "x": 40, "y": 200, "z": 5, "tag": "castle", "collider": new DE.CircleCollider( 20 )
-	    } );
-		
-	    this.life = 2000;
-
+	    this.add( new DE.GameObject( {
+		      "x": 0, "y": 40, "z": 5, "tag": "castle2", "collider": new DE.CircleCollider( 60 )
+		    } ) );
 
 	    this.init = function(){
-	    	    Game.scene.add( castle1 );
-	    	    Game.scene.add( castle2 );
-	    	    Game.scene.add( castle3 );
-	    	    Game.scene.add( castle4 );
-	    	    Game.scene.add( castle5 );
+
+		    var castle1 = new DE.GameObject( {
+		      "x": 110, "y": 500, "z": 5, "tag": "castle", "collider": new DE.CircleCollider( 60 )
+		    }  );
+
+		    var castle2 = new DE.GameObject( {
+		      "x": 110, "y": 600, "z": 5, "tag": "castle", "collider": new DE.CircleCollider( 60 )
+		    }  );
+
+		    var castle3 = new DE.GameObject( {
+		      "x": 110, "y": 700, "z": 5, "tag": "castle", "collider": new DE.CircleCollider( 60 )
+		    }  );
+
+		    var castle4 = new DE.GameObject( {
+		      "x": 110, "y": 300, "z": 5, "tag": "castle", "collider": new DE.CircleCollider( 60 )
+		    } );
+
+		    var castle5 = new DE.GameObject( {
+		      "x": 110, "y": 200, "z": 5, "tag": "castle", "collider": new DE.CircleCollider( 60 )
+		    } );
+			
+
+	    	    this.scene.add( castle1 );
+	    	    this.scene.add( castle2 );
+	    	    this.scene.add( castle3 );
+	    	    this.scene.add( castle4 );
+	    	    this.scene.add( castle5 );
+	    	    console.log("salut");
 	    	}
 
 	}
+	Castle.prototype = new DE.GameObject();
+  	Castle.prototype.constructor = Castle;
+  	Castle.prototype.supr        = DE.GameObject.prototype;
+
+
 	return Castle;
 } );
--- _dev/js/custom/Trap.js
+++ _dev/js/custom/Trap.js
@@ -8,7 +8,7 @@ define( ['DREAM_ENGINE'], function(DE)
 		this.startpos = {x: 212 * 0.67, y:209 * 0.6666666666666666666666666666666};
 		this.life = 100;
 
-		console.log((this.startpos.x + (this.idCase % 8) * this.width) + " " + (this.startpos.y + (Math.floor( this.idCase / 6) * this.heigth)))
+		
 		DE.GameObject.call(this,
 		{
 	      "x": this.startpos.x + ((this.idCase % 10) * this.width) + this.width / 2,
@@ -17,7 +17,7 @@ define( ['DREAM_ENGINE'], function(DE)
 	      "renderer": new DE.SpriteRenderer( { "spriteName": "heart", "scale": 1 * 0.6666666666666666666 } ),
 	      "collider": new DE.CircleCollider( this.width/2 )
 	    });
-	    console.log(this.position.x + " " + this.position.y);
+
 	}
 
 	Trap.prototype = new DE.GameObject();

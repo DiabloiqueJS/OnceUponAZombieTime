f0f7c3090058c2146251016ca79e840ca74c8470
stara.alexis <stara.alexis.isart@gmail.com>
2014-05-10 13:51:13 +0200

    Des trucs parce que emma elle dit des betises

--- _dev/js/custom/Trap.js
+++ _dev/js/custom/Trap.js
@@ -5,31 +5,65 @@ define( ['DREAM_ENGINE'], function(DE)
 		this.idCase = idCase
 		this.width = 117;
 		this.heigth = 117;
-		this.startpos = ;
+		this.startpos = {x: 212, y:209};
+		this.life = 100;
 
 		DE.GameObject.call(this,
 		{
-	      "x": startpos + ((this.idCase % 8) * this.width), 
-	      "y": startpos + (Math.floor( this.idCase / 8) * this.heigth), 
+	      "x": startpos.x + ((this.idCase % 8) * this.width),
+	      "y": startpos.y + (Math.floor( this.idCase / 8) * this.heigth),
 	      "tag": this.comportement,
 	      "renderer": new DE.SpriteRenderer( { "spriteName":??????, "startFrame": 0, "scale": ???????? || 1 } ),
-	      "collider": new DE.FixedBoxCollider( this.width, this.heigth )
+	      "collider": new DE.CircleCollider( this.width/2 )
 	    });
 
-		switch(comportement)
+	    
+    	switch(comportement)
 		{
 			case "hole":
-				
+				this.activate = function (zombies)
+    			{
+    				for (var i = 0; i <= zombies.length; i++) 
+    				{
+	    				if (DE.CollisionSystem.circleCollision( this.collider, zombies[i].collider )) 
+	    				{
+	    					zombies[i].life = 0;
+	    				}
+    				}
+    			}
 				break;
 			case "wall":
-				
+				this.activate = function ()
+    			{
+    				for (var i = 0; i <= zombies.length; i++) 
+    				{
+	    				if (DE.CollisionSystem.circleCollision( this.collider, zombies[i].collider )) 
+	    				{
+	    					zombies[i].?????;
+	    				}
+    				}
+    			}
 				break;
 			case "stab":
-				
+				this.activate = function ()
+    			{
+    				for (var i = 0; i <= zombies.length; i++) 
+    				{
+	    				if (DE.CollisionSystem.circleCollision( this.collider, zombies[i].collider )) 
+	    				{
+	    					zombies[i].life --;
+	    				}
+    				}
+    			}
+				break;
+			case "normal"
 				break;
 			case default:
 				break;
 		}
+	    
+
+	    this.addAutomatism( "IATrap", "activate" );
 	}
 
 	Trap.prototype = new DE.GameObject();
